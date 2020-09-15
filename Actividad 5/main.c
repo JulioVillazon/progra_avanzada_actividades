5e00819784d9e3fcd6f45fa78d6955bac4876884
Jesus Gonzalez <jesus.gonzalez.ac@hotmail.com>
2020-09-15 15:08:33 -0500

    Update main.c

--- Actividad 5/main.c
+++ Actividad 5/main.c
@@ -44,47 +44,50 @@ int main(int argc, char *argv[])
     process_t *final = vector + nProc; 
 
     char witness = 'T';
-    
 
-    while(aux < final)
-    {        
-        pid_t pId;                        
-        int status;
+    int tubo[2*nProc];
+    pipe(tubo);
+    int n = 0;
+
+    for(; aux < final; ++aux){
+
+        pid_t pId;
         pId = fork();
+        int status;
+        int cycle = 0;
 
-        int tubo[2];
-        pipe(tubo);
-
-        if (pId == -1)
+        while (cycle < nProc)
         {
-            printf("Could not create child process\n");
-            return -1;
+            if (pId == -1)
+            {
+                printf("Could not create child process\n");
+                return -1;
+            }
+            else if (pId == 0)
+            {
+
+                close(tubo[n+1]);
+                read(tubo[n], &witness, sizeof(char));
+                aux->p_id = getpid();
+                printf("—->Soy el proceso con PID %d y recibí el testigo %c, el cual tendré por 5 segundos. \n", aux->p_id, witness);
+                sleep(5);
+
+                n+=2 ;
+
+                printf("<—- Soy el proceso con PID %d  y acabo de enviar el testigo %c. \n", aux->p_id, witness);
+                close(tubo[n]);
+                write(tubo[n+1], &witness, sizeof(char));
+                exit(0);
+            }
+            else
+            {
+                ++cycle;
+                wait(&status);
+            }
         }
-        else if (pId == 0)
-        {
-            close(tubo[1]);
-            read(tubo[0], &witness, sizeof(char));
-            aux->p_id = getpid();
-            printf("—->Soy el proceso con PID %d y recibí el testigo %c, el cual tendré por 5 segundos. \n", aux->p_id, witness);
-            sleep(5);
+    }
 
-            printf("<—- Soy el proceso con PID %d  y acabo de enviar el testigo %c. \n", aux->p_id, witness);
-            close(tubo[0]);
-            write(tubo[1], &witness, sizeof(char));
-                      
-            exit(0);
-        } 
-        else
-        {
-            close(tubo[1]);
-            read(tubo[0], &witness, sizeof(char));  
-            wait(&status);               
-        }       
-        ++aux;
-        // if(aux == final){
-        //     aux = vector;
-        // }
-    }          
+           
 
 
     free(vector);
