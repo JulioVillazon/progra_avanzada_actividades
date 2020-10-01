7b1d89b3901cce21414cb30713fb43accf32060f
Jesus Gonzalez <jesus.gonzalez.ac@hotmail.com>
2020-10-01 08:52:03 -0500

    .

--- Actividad 6/client.c
+++ Actividad 6/client.c
@@ -7,47 +7,51 @@
 
 #define TCP_PORT 8000
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
     struct sockaddr_in direccion;
+
     char buffer[1000];
-    
+
     int cliente;
-    
+
     ssize_t leidos, escritos;
-    
-    if (argc != 2) {
+
+    if (argc != 2)
+    {
         printf("Use: %s IP_Servidor \n", argv[0]);
         exit(-1);
     }
-    
+
     // Crear el socket
     cliente = socket(PF_INET, SOCK_STREAM, 0);
-    
+
     // Establecer conexión
     inet_aton(argv[1], &direccion.sin_addr);
     direccion.sin_port = htons(TCP_PORT);
     direccion.sin_family = AF_INET;
-    
-    escritos = connect(cliente, (struct sockaddr *) &direccion, sizeof(direccion));
-    
-    if (escritos == 0) {
+
+    escritos = connect(cliente, (struct sockaddr *)&direccion, sizeof(direccion));
+
+    if (escritos == 0)
+    {
         printf("Conectado a %s:%d \n",
                inet_ntoa(direccion.sin_addr),
                ntohs(direccion.sin_port));
-        
+
         // Escribir datos en el socket
-        while ((leidos = read(fileno(stdin), &buffer, sizeof(buffer)))) {
+        while ((leidos = read(fileno(stdin), &buffer, sizeof(buffer))))
+        {
             write(cliente, &buffer, leidos);
-            
+
             /* Lee del buffer y escribe en pantalla */
-            leidos = read(cliente, &buffer, sizeof(buffer) );
+            leidos = read(cliente, &buffer, sizeof(buffer));
             write(fileno(stdout), &buffer, leidos);
         }
     }
-    
+
     // Cerrar sockets
     close(cliente);
-    
+
     return 0;
 }
--- Actividad 6/server.c
+++ Actividad 6/server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,78 +8,91 @@
 
 #define TCP_PORT 8000
 
-int main(int argc, const char * argv[])
+void gestor_ctrl(int);
+
+int main(int argc, const char *argv[])
 {
+
     struct sockaddr_in direccion;
     char buffer[1000];
-    
+
     int servidor, cliente;
-    
-    ssize_t leidos, escritos;
-    int continuar = 1;
-    pid_t pid;
-    
-    if (argc != 2) {
-        printf("Use: %s IP_Servidor \n", argv[0]);
+
+    int leidos, escritos;
+
+    struct sigaction senal;
+
+    if (argc != 2)
+    {
+        printf("Error. Use: %s A.B.C.D \n", argv[0]);
         exit(-1);
     }
-    
-    // Crear el socket
+
+    //Crear el socket
     servidor = socket(PF_INET, SOCK_STREAM, 0);
-    
+
     // Enlace con el socket
-    inet_aton(argv[1], &direccion.sin_addr);
     direccion.sin_port = htons(TCP_PORT);
     direccion.sin_family = AF_INET;
-    
-    bind(servidor, (struct sockaddr *) &direccion, sizeof(direccion));
-    
-    // Escuhar
+    inet_aton(argv[1], &direccion.sin_addr);
+
+    bind(servidor, (struct sockaddr *)&direccion, sizeof(direccion));
+
+    // Escuchar a los clientes
     listen(servidor, 10);
-    
-    escritos = sizeof(direccion);
-    
-    // Aceptar conexiones
-    while (continuar)
+
+    // Comunicación
+    socklen_t tamano = sizeof(direccion);
+
+    cliente = accept(servidor, (struct sockaddr *)&direccion, &tamano);
+
+    sigaction(SIGTSTP, NULL, &senal);
+    sigaction(SIGINT, NULL, &senal);
+
+    if (cliente >= 0)
     {
-        cliente = accept(servidor, (struct sockaddr *) &direccion, &escritos);
-        
         printf("Aceptando conexiones en %s:%d \n",
                inet_ntoa(direccion.sin_addr),
                ntohs(direccion.sin_port));
-        
-        pid = fork();
-        
-        if (pid == 0) continuar = 0;
-        
-    }
-    
-    if (pid == 0) {
-        
-        close(servidor);
-        
-        if (cliente >= 0) {
-            
-            // Leer datos del socket
-            while (leidos = read(cliente, &buffer, sizeof(buffer))) {
-                write(fileno(stdout), &buffer, leidos);
-                
-                /* Leer de teclado y escribir en el socket */
-                leidos = read(fileno(stdin), &buffer, sizeof(buffer));
-                write(cliente, &buffer, leidos);
-            }
+
+        senal.sa_handler = gestor_ctrl;
+
+        if (sigaction(SIGTSTP, &senal, 0) == -1 || sigaction(SIGINT, &senal, 0) == -1)
+        {
+            printf("Error en el gestor de señales.\n");
+            exit(-1);
         }
+
         
-        close(cliente);
+        // Leer de socket y escribir en pantalla
+        while (leidos = read(cliente, &buffer, sizeof(buffer)))
+        {
+            write(fileno(stdout), &buffer, leidos);
+
+            leidos = read(fileno(stdin), &buffer, sizeof(buffer));
+            write(cliente, &buffer, leidos);
+        }
     }
-    
-    else if (pid > 0)
-    {
-        while (wait(NULL) != -1);
-        
-        // Cerrar sockets
-        close(servidor);
-        
-    }
+
+    // Cerrar el socket
+
+    sigaction(SIGTSTP, &senal, NULL);
+    sigaction(SIGINT, &senal, NULL);
+
+    close(servidor);
+    close(cliente);
+
     return 0;
+}
+
+void gestor_ctrl(int sid)
+{
+    if (sid == SIGTSTP)
+    {
+        printf("%d\n", sid);
+    }
+    else if (sid == SIGINT)
+    {
+        printf("%d\n", sid);
+    }
 }
