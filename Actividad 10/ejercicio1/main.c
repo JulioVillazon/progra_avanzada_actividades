c843bc156a94b3685e3077002b00ee8908440015
JesusC10 <jesus.gonzalez.ac@hotmail.com>
2020-10-29 07:37:31 -0600

    .

--- Actividad 10/ejercicio1/main.c
+++ Actividad 10/ejercicio1/main.c
@@ -17,11 +17,11 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 sem_t *chair, *food;
 #else
 /* Para Linux */
-sem_t men, women;
+sem_t chair, food;
 #endif
 
 int dwarfs_waiting = 0;
-//Blsncanieves tiene 2 estados 0 = dando una vuelta, 1 = sirviendo
+//Blancanieves tiene 2 estados 0 = dando una vuelta, 1 = sirviendo
 int snowwhite_state = 0;
 
 void *snowhite(void *);
@@ -38,8 +38,8 @@ int main(int argc, const char *argv[])
     food = sem_open("food", O_CREAT, 777, 0);
 #else
     /* Inicializar semáforos en Linux */
-    sem_init(&men, 0, 0);
-    sem_init(&women, 0, 0);
+    sem_init(&chair, 0, CHAIRS);
+    sem_init(&food, 0, 0);
 #endif
 
     /* Crear a los babuinos */
@@ -60,14 +60,14 @@ int main(int argc, const char *argv[])
         pthread_join(threads[i], NULL);
     }
 
-#ifdef __APPLE__
+    #ifdef __APPLE__
         /* En MacOS*/
         sem_close(chair);
         sem_close(food);
     #else
         /* Liberar los recursos en Linux */
-        sem_destroy(&men);
-        sem_destroy(&women);
+        sem_destroy(&chair);
+        sem_destroy(&food);
     #endif
 
     pthread_exit(NULL);
@@ -75,50 +75,66 @@ int main(int argc, const char *argv[])
 
 void *dwarf(void *arg)
 {
-    int dwarfs_seated = 0;
+    int available_chairs;
+    int total_dwarfs = 0;
+    int eating;
 
-    while (1)
+    #ifdef __APPLE__
+        /* Para MacOS */
+        sem_getvalue(chair, &available_chairs);
+        sem_getvalue(food, &eating);
+    #else
+        /* Inicializar semáforos en Linux */
+        sem_getvalue(&chair, &available_chairs);
+        sem_getvalue(&food, &eating);
+    #endif
+
+    while (total_dwarfs < DWARFS)
     {
-        sleep(rand() % 3);
+        sleep(rand() % 5);
 
-        printf("(D) Regreso un enanito.(%d enanitos esperando) \n", ++dwarfs_waiting);
+        printf("(E) Regreso un enanito.\n");
+        ++dwarfs_waiting;
+        ++total_dwarfs;
 
-        pthread_mutex_lock(&mutex);
 
-        if(dwarfs_seated < CHAIRS){
-            sem_post(chair);
-            
-            printf("(D) Se sento un enanito (%d enanitos en la mesa) \n", ++dwarfs_seated);
-            if(dwarfs_waiting > 0){
+        if(available_chairs > 0){
+            #ifdef __APPLE__
+                /* Para MacOS */
+                sem_wait(chair);
+                sem_getvalue(chair, &available_chairs);
+            #else
+                /* Inicializar semáforos en Linux */
+                sem_wait(&chair);
+                sem_getvalue(&chair, &available_chairs);
+            #endif
+
+            printf("(E) Se sento un enanito (%d sillas disponibles) \n", available_chairs);
+            if (dwarfs_waiting > 0)
+            {
                 --dwarfs_waiting;
             }
-        }else{
-            printf("(D) Las sillas estan ocupadas. \n");
+            snowwhite_state = 1;
+ 
+        }
+        else{
+            printf("(E) Las sillas estan ocupadas. (%d enanitos esperando) \n", dwarfs_waiting);
         }
 
-        pthread_mutex_unlock(&mutex);
+        // pthread_mutex_unlock (&mutex);
 
-        
-
-        // if (toilet_state == 0)
-        // {
-        //     toilet_state = 1;
-        //     printf("(M) Sanitario ocupado por mujeres.\n");
-        // }
-        // if (toilet_state == 1)
-        // {
-        //     --waiting_women;
-        //     printf("(M) Entra una mujer (%d en espera).\n", waiting_women);
-
-        //     #ifdef __APPLE__
-        //                 sem_post(women);
-        //     #else
-        //                 sem_post(&women);
-        //     #endif
-        // }
-        // pthread_mutex_unlock(&mutex_toilet);
-
-        // ++arrivedwomen;
+        if (eating > 0)
+        {
+            #ifdef __APPLE__
+                /* Para MacOS */
+                sem_wait(food);
+                sem_getvalue(food, &eating);
+            #else
+                /* Inicializar semáforos en Linux */
+                sem_wait(&food);
+                sem_getvalue(&food, &eating);
+            #endif
+        }
     }
 
     pthread_exit(NULL);
